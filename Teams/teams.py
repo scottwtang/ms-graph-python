f404f7bd3e146358007923162d5e768822317fa5
Scott Tang <48874569+scottwtang@users.noreply.github.com>
2022-05-20 19:59:31 -0400

    Update teams.py

--- Teams/teams.py
+++ Teams/teams.py
@@ -1,4 +1,24 @@
-def send_message_attachment(chat_id, attachment_name, attachment_path, attachment_id, access_token, chat_message = None):
+def send_chat_message(access_token, chat_id: str, msg_content: str = ""):
+    # This function sends a Teams chat message, see Example 1
+    # https://docs.microsoft.com/en-us/graph/api/chatmessage-post?view=graph-rest-1.0&tabs=http#example-1-send-a-hello-world-message-in-a-channel
+
+    request_url = f"https://graph.microsoft.com/v1.0/chats/{chat_id}/messages"
+
+    request_headers = {
+            "Authorization": "Bearer " + access_token,
+            "Content-Type": "application/json"
+        }
+
+    request_body = {
+        "body": {
+            "content": msg_content
+        }
+    }
+
+    result = requests.post(url = request_url, headers = request_headers, json = request_body)
+    return(result)
+
+def send_chat_message_attachment(access_token: str, chat_id: str, attachment_name: str, attachment_path: str, attachment_id: str, msg_content: str = ""):
     # This function sends a Teams chat message with an attachment, see Example 4
     # https://docs.microsoft.com/en-us/graph/api/chatmessage-post?view=graph-rest-1.0&tabs=http#example-4-send-a-message-with-file-attachment-in-it
 
@@ -12,7 +32,7 @@ def send_message_attachment(chat_id, attachment_name, attachment_path, attachmen
     request_body = {
         "body": {
             "contentType": "html",
-            "content": f"{chat_message} <attachment id=\"{attachment_id}\"></attachment>"
+            "content": f"{msg_content} <attachment id=\"{attachment_id}\"></attachment>"
         },
         "attachments": [
             {
