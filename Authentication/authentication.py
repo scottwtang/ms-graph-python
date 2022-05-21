bad3f82dcd6bbd7563fa8c6c26c6bdd3e7a39c9d
Scott Tang <48874569+scottwtang@users.noreply.github.com>
2022-05-20 20:15:03 -0400

    Create authentication.py

---
+++ Authentication/authentication.py
@@ -0,0 +1,16 @@
+import msal
+
+def get_token_with_client_secret(client_id: str, client_secret: str, tenant_id: str):
+    # This function is to obtain a bearer token using the client credentials flow, with a client secret instead of a certificate
+    # https://docs.microsoft.com/en-us/graph/sdks/choose-authentication-providers?tabs=CS#client-credentials-provider
+    
+    app = msal.ConfidentialClientApplication(
+        client_id         = client_id,
+        client_credential = client_secret,
+        authority         = f"https://login.microsoftonline.com/{tenant_id}")
+
+    scopes = ["https://graph.microsoft.com/.default"]
+
+    token = app.acquire_token_for_client(scopes = scopes)
+
+    return(token)
